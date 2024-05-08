bb6c52f71c300b37a8a30797edfc2f5a30b4c8d5
I don't know what to name it <deontre813@gmail.com>
2024-05-07 22:49:08 -0400

    1

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+MakeCacheMatrix <- function(m = matrix()) {
+  i <- NULL
+  set <- function(x) {
+    m <<- x
+    i <<- NULL
+  }
+  get <- function() m
+  setinv <- function(inverse) i <<- inverse
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinverse,
+       getinv = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+  i <- m$getinv()
+  if(!is.null(i)) {
+    message("retrieving cached data")
+    return(i)
+  }
+  matrix_to_invert <- m$get()
+  i <- solve(matrix_to_invert, ...)
+  m$setinv(i)
+  i
 }
